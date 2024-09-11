9340e24178f56d561d584c066e05573d5853facd
MA-Silent <38739@ma-web.nl>
2024-09-11 17:00:07 +0200

    ;

--- Player/Player.js
+++ Player/Player.js
@@ -1,22 +1,22 @@
-let Name = "Ben Ruitenbeek"
-let jF = 20
-let sP = "Time Slowdown"
-let S = 280
-let B = "Honda VFR VTEC"
-let lL = 2
-let A = 2000
-let NoL = 3
-let bP = 100
-let L = 1
-var wanted = false
-console.log("Name: "+ Name)
-console.log("JumpForce: "+jF)
-console.log("SpecialPower: "+ sP)
-console.log("Speed: "+ S)
-console.log("Bike: "+ B)
-console.log("Lives left: "+ lL)
-console.log("Ammo: "+ A)
-console.log("NumberOfLasers: "+ NoL)
-console.log("BatteryPower"+ bP)
-console.log("Level: "+ L)
-console.log("Wanted: "+ wanted)
+let Name = "Ben Ruitenbeek";
+let jF = 20;
+let sP = "Time Slowdown";
+let S = 280;
+let B = "Honda VFR VTEC";
+let lL = 2;
+let A = 2000;
+let NoL = 3;
+let bP = 100;
+let L = 1;
+var wanted = false;
+console.log("Name: "+ Name);
+console.log("JumpForce: "+jF);
+console.log("SpecialPower: "+ sP);
+console.log("Speed: "+ S);
+console.log("Bike: "+ B);
+console.log("Lives left: "+ lL);
+console.log("Ammo: "+ A);
+console.log("NumberOfLasers: "+ NoL);
+console.log("BatteryPower"+ bP);
+console.log("Level: "+ L);
+console.log("Wanted: "+ wanted);
---
+++ Test.bat
@@ -0,0 +1,4 @@
+@echo off 
+node ./Name_Age_Docent/Name_Age_Docent.js
+node ./Player/Player.js
+node ./Tijd/Tijd.js
