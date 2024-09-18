d78e05938434dfc273abd10d6fbfd5281179bfdc
MA-Silent <38739@ma-web.nl>
2024-09-18 15:52:04 +0200

    enter change

--- JS_AND_OR_IF/opdracht.js
+++ JS_AND_OR_IF/opdracht.js
@@ -5,9 +5,7 @@ if(day == 6){
     dayOfWeek = "zaterdag"
 }else if(day == 7){
     dayOfWeek = "zondag"
-}
-
-if(dayOfWeek == "zaterdag" || dayOfWeek == "zondag"){
+}if(dayOfWeek == "zaterdag" || dayOfWeek == "zondag"){
     console.log("Lekker uitslapen!");
 }else{
     console.log("Ik moet naar school!!! Kut");
