5315acf85ceadc887d08a933662a8cb92133552b
MA-Silent <38739@ma-web.nl>
2024-09-18 15:16:02 +0200

    Opdracht JS or if else if

---
+++ JS_AND_OR_IF/opdracht.js
@@ -0,0 +1,9 @@
+const huidigeDag= "zondag";
+
+if(huidigeDag == "maandag" || huidigeDag == "dinsdag" ||huidigeDag == "woensdag" || huidigeDag == "donderdag"){
+    console.log("Ik moet naar school");
+}else if(huidigeDag == huidigeDag == "vrijdag"){
+    console.log("Laaste dag naar school");
+}else {
+    console.log("Lekker uitslapen!");
+}
