f23eb58d17d4319cb227b3387e94555e08ac3d7b
MA-Silent <38739@ma-web.nl>
2024-10-02 14:08:42 +0200

    fixes

--- Wis/index.js
+++ Wis/index.js
@@ -1,5 +1,19 @@
 function brekenKubus(a, b, c){
     return a*b*c;
 }
+function brekenCylinder(hoogte, straal){
+    var intermediary = Math.PI*straal*straal*hoogte;
+     var math = intermediary.toFixed(2);
+    return math + " cm3"
+}
+function brekenRHDriehoek(a, b, c){
+    return a*b*c;
+}
+function averageOfSeven(a, b, c, d, e, f, g){
+    return a+b+c+d+e+f+g/7;
+}
 let kubus = brekenKubus(10,5,2);
-console.log(kubus);
+let average = averageOfSeven(1,2,3,4,5,6,7);
+let cylinder = brekenCylinder(200,200);
+let RHdriehoek = brekenRHDriehoek();
+console.log(kubus,average,cylinder,RHdriehoek);
