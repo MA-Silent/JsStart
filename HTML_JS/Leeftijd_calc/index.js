777cfb65b6035601f071b90a094429ee81d37a6c
MA-Silent <38739@ma-web.nl>
2024-10-02 15:38:59 +0200

    Remove

--- HTML_JS/Leeftijd_calc/index.js
+++
@@ -1,9 +0,0 @@
-let now = new Date()
-let outP = document.getElementById("out")
-let x = document.forms["myForm"]["fname"].value;
-outP.innerHTML = "" + y
-// function calc(y,m,d){
-//     let birth = new Date(y,m,d); 
-//     let birthDay = birth - now;
-//     outP.innerHTML = "You are:" + birthDay;
-// }
--- Wis/index.js
+++ Wis/index.js
@@ -1,17 +1,17 @@
 function brekenKubus(a, b, c) {
     var intermediary = a * b * c;
     var math = intermediary.toFixed(2);
-    return math
+    return math;
 }
 function brekenCylinder(hoogte, straal) {
     var intermediary = Math.PI * straal**2 * hoogte;
     var math = intermediary.toFixed(2);
-    return math + " cm3"
+    return math + " cm3";
 }
 function brekenRHDriehoek(a, b) {
     var intermediary = Math.sqrt(a ** 2 + b ** 2);
     var math = intermediary.toFixed(2);
-    return math
+    return math;
 }
 
 function averageOfSeven(a, b, c, d, e, f, g) {
@@ -22,5 +22,5 @@ function averageOfSeven(a, b, c, d, e, f, g) {
 let kubus = brekenKubus(10, 5, 2);
 let average = averageOfSeven(1, 2, 3, 4, 5, 6, 7);
 let cylinder = brekenCylinder(200, 200);
-let RHdriehoek = brekenRHDriehoek(2,2);
+let RHdriehoek = brekenRHDriehoek(10,5);
 console.log(kubus, average, cylinder, RHdriehoek);
