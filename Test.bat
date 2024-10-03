1328aaca0d2640b685b34f8d4da2c6da021651f4
MA-Silent <38739@ma-web.nl>
2024-10-03 12:30:22 +0200

    Test.bat

--- Test.bat
+++ Test.bat
@@ -5,3 +5,10 @@ echo Player.js -----------------------------------
 node ./Player/Player.js
 echo Tijd.js -----------------------------------
 node ./Tijd/Tijd.js
+echo Wiskunde.js -----------------------------------
+node ./Wis/index.js
+echo Age.js -----------------------------------
+node ./Age_calc/Age.js
+pause >>del
+del del
+cls
--- Wis/index.js
+++ Wis/index.js
@@ -17,7 +17,7 @@ function brekenRHDriehoek(a, b) {
 function averageOfSeven(a, b, c, d, e, f, g) {
     var intermediary = (a + b + c + d + e + f + g) / 7;
     var math = intermediary.toFixed(2);
-    return math
+    return math;
 }
 let kubus = brekenKubus(10, 5, 2);
 let average = averageOfSeven(1, 2, 3, 4, 5, 6, 7);
