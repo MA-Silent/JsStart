b74cd8980b433ee871dbc95e21af6c0a3a46e130
MA-Silent <38739@ma-web.nl>
2024-10-03 12:33:06 +0200

    ; fix

--- Age_calc/Age.js
+++ Age_calc/Age.js
@@ -6,11 +6,11 @@ function Age_calc(j, m, d){
     let now = new Date();
     let birth = new Date(j,m,d);
     let ms = now - birth;
-    let out = ms/31557600000
-    return out.toFixed(2)
+    let out = ms/31557600000;
+    return out.toFixed(2);
 }
 
-console.warn("Je bent "+Age_calc(jaar,maand,dag)+ " jaar oud")
+console.warn("Je bent "+Age_calc(jaar,maand,dag)+ " jaar oud");
 console.log("Je geboorte jaar is: " + jaar);
 console.log("Je geboorte maand is: " + maand);
 console.log("Je geboorte dag is: " + dag);
